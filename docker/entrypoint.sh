20e71eb93a0be2f9f065e331ffb438de1a5bfda5
toko0715 <strongsitojheremy@gmail.com>
2026-06-26 00:04:07 -0500

    fix loop

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,6 @@ touch /var/www/html/storage/database.sqlite
 
 sed -ri "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -ri "s/<VirtualHost \*:80>/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
-sed -ri "s!DocumentRoot /var/www/html!DocumentRoot /var/www/html/public!" /etc/apache2/sites-available/000-default.conf
 
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
